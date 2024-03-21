944ba30ab88969ab8858aff5b0fb6a32c9059b72
Shivani-Chouksey <122255940+Shivani-Chouksey@users.noreply.github.com>
2024-03-21 13:51:25 +0530

    four

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,7 +9,7 @@ export default function MyDocument() {
         <link rel="icon" href="#" />
         <link
           rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
         />
         <link
           rel="stylesheet"
